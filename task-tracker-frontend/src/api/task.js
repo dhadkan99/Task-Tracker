6532e0785d4e23a39a3b8bb9e1fd1875d15784bc
Dhadkan <dhadkankc88@gmail.com>
2025-07-04 18:18:39 +0545

    delete

--- task-tracker-frontend/src/api/task.js
+++ task-tracker-frontend/src/api/task.js
@@ -1,7 +1,21 @@
-const API_URL = "http://localhost:5000/api/tasks";
-
+const API_URL = 'http://localhost:5000/api/tasks';
 export const fetchTasks = async () => {
-  const response = await fetch(API_URL);
-  if (!response.ok) throw new Error("failed to fetch task!");
-  return await response.json();
+const response = await fetch(API_URL);
+return await response.json();
+};
+
+export const addTask = async (text) => {
+const response = await fetch(API_URL, {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({ text })
+});
+return await response.json();
+};
+
+export const deleteTask = async (id) => {
+const response = await fetch(`${API_URL}/${id}`, {
+method: 'DELETE'
+});
+return await response.json();
 };
--- task-tracker-frontend/src/components/Footer.jsx
+++ task-tracker-frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="py-4 text-center text-black bg-white/30 backdrop-blur-md border-b border-gray-300 shadow-sm">
+    <footer className="py-4 text-center text-black border-b border-gray-300 shadow-sm backdrop-blur-md bg-white/30">
       <p>@{new Date().getFullYear()} TaskTracker. All rights reserved.</p>
     </footer>
   );
