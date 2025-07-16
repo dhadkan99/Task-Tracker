66c6df5ac10b96475dc539a2b559d49de233cd47
Dhadkan <dhadkankc88@gmail.com>
2025-07-16 08:51:04 +0545

    databese

--- task-tracker-backend/users.js
+++ task-tracker-backend/users.js
@@ -1,12 +1,12 @@
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
-const users = [
-  {
-    id: '1',
-    email: 'user@example.com',
-    password: bcrypt.hashSync('password123', 10), // Hashed password
-    role: 'user',
-  },
-];
+// const users = [
+//   {
+//     id: '1',
+//     email: 'user@example.com',
+//     password: bcrypt.hashSync('password123', 10), // Hashed password
+//     role: 'user',
+//   },
+// ];
 
-module.exports = users;
+// module.exports = users;
