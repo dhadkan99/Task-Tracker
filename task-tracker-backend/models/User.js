2e69e79f11b9a1d837621c0aec122339c59740f1
Dhadkan <dhadkankc88@gmail.com>
2025-07-23 07:34:26 +0545

    bakcend

---
+++ task-tracker-backend/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+
+    role: { type: String, required: true, default: "User" },
+    googleID: { type: String, required: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
