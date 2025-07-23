3ab4dc3a3338360f2c209e5b3c1282529b3b159b
Dhadkan <dhadkankc88@gmail.com>
2025-07-23 12:34:25 +0545

    wwithout google auth

--- task-tracker-frontend/src/App.js
+++ task-tracker-frontend/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./pages/Dashboard";
 import { ToastContainer } from "react-toastify";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
